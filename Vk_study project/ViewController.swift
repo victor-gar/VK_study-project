c02111db557afc01beb792a4511a676877856611
victor-gar <dinomc4711@gmail.com>
2022-09-02 16:01:35 +0600

    Пользовательский интерфейс iOS-приложений

---
+++ Vk_study project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vk_study project
+//
+//  Created by Victor Garitskyu on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
